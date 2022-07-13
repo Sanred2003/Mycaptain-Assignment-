6b417a74fe81f28be52324a75233b81c6f282a1d
Sanred2003 <109232835+Sanred2003@users.noreply.github.com>
2022-07-13 21:24:15 +0530

    Task 2.py
    write a Python program to accept a filename from the user and print the extension of that.

--- Task 2.py
+++ Task 2.py
@@ -1,4 +1,3 @@
-# write a Python program to accept a filename from the user and print the extension of that.
 ilename = input("Input the Filename: ")
 f_extns = filename.split(".")
 print ("The extension of the file is : " + repr(f_extns[-1]))
