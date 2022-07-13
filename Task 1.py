819c90ff28d1e890c40d1b9335ff41c2485a7f93
Sanred2003 <109232835+Sanred2003@users.noreply.github.com>
2022-07-13 21:23:03 +0530

    Task 1.py
    write a Python program that accepts the radius of a circle from the user and computes the area.

--- Task 1.py
+++ Task 1.py
@@ -1,4 +1,3 @@
-#write a Python program which accepts the radius of a circle from the user and computes area.
 r = float(input("Enter radius of circle: "))
 a = 3.14159 * r * r
 print("Area of circle =", a)
